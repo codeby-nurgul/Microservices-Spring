106e8a54bc928def4cb80747fa88a3e6221fa246
Nurgül Yalman <123768429+nurshahs@users.noreply.github.com>
2026-06-14 12:48:31 +0300

    securityconfig resource server eklendi

--- microservices/product-service/src/main/java/com/turkcell/product_service/controller/ProductsController.java
+++ microservices/product-service/src/main/java/com/turkcell/product_service/controller/ProductsController.java
@@ -15,7 +15,7 @@ import com.turkcell.product_service.entity.TestClass;
 import com.turkcell.product_service.event.TestEvent;
 import com.turkcell.product_service.repository.OutboxRepository;
 
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 @RestController
 public class ProductsController {
     private final OutboxRepository outboxRepository;
@@ -24,6 +24,11 @@ public class ProductsController {
     public ProductsController(OutboxRepository outboxRepository, ObjectMapper objectMapper) {
         this.outboxRepository = outboxRepository;
         this.objectMapper = objectMapper;
+    }
+
+    @GetMapping()
+    public String hello() {
+        return "Hello";
     }
 
     @GetMapping("/test")
@@ -37,15 +42,16 @@ public class ProductsController {
         UUID id = UUID.randomUUID();
         UUID eventId = UUID.randomUUID();
         var event = new TestEvent(eventId, message, id);
-        //streamBridge.send("testEvent-out-0", event);
+        // streamBridge.send("testEvent-out-0", event);
 
         // KAFKAYA bir event gidecekse, önce kayıt altına alınacak.
         // Outbox -> XEvent,XTarihi,XTopic,XPayload
-        
 
         // Daha sonra bir mekanizma bu kayıtları okuyacak ve kafkaya gönderecek.
-        // POLLING -> Belirli aralıklarla veritabanaına bak, gönderilecek bir event var mı?
-        // Her 20 snde => SElect * from outbox where status = 'PENDING' and retryCount < 3
+        // POLLING -> Belirli aralıklarla veritabanaına bak, gönderilecek bir event var
+        // mı?
+        // Her 20 snde => SElect * from outbox where status = 'PENDING' and retryCount <
+        // 3
         // CDC (Change Data Capture)
         // Debezium gibi bir mekanizma
 
@@ -63,9 +69,11 @@ public class ProductsController {
         return "Başarılı";
     }
 
-    private String toJson(Object o)
-    {
-        try { return objectMapper.writeValueAsString(o);}
-        catch(Exception e) { throw new RuntimeException(e); }
+    private String toJson(Object o) {
+        try {
+            return objectMapper.writeValueAsString(o);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
